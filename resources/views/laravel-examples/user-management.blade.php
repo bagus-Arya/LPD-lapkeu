f7e48e9d1feca69941a561bec24864b2e56d63d1
agum aditya <agumaditya71@gmail.com>
2022-10-23 21:54:42 +0800

    lol

--- resources/views/laravel-examples/user-management.blade.php
+++ resources/views/laravel-examples/user-management.blade.php
@@ -10,6 +10,142 @@
             <a href="https://www.creative-tim.com/live/soft-ui-dashboard-pro-laravel" target="_blank" class="text-white">here</a></strong>
             to see the PRO product!
         </span>
+    </div> -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title font-weight-normal" id="exampleModalLabel">Tambah User</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="/user-profile" method="POST" role="form text-left">
+                    @csrf
+                    @if($errors->any())
+                        <div class="mt-3  alert alert-primary alert-dismissible fade show" role="alert">
+                            <span class="alert-text text-white">
+                            {{$errors->first()}}</span>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                                <i class="fa fa-close" aria-hidden="true"></i>
+                            </button>
+                        </div>
+                    @endif
+                    @if(session('success'))
+                        <div class="m-3  alert alert-success alert-dismissible fade show" id="alert-success" role="alert">
+                            <span class="alert-text text-white">
+                            {{ session('success') }}</span>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                                <i class="fa fa-close" aria-hidden="true"></i>
+                            </button>
+                        </div>
+                    @endif
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="fullname" class="form-control-label">{{ __('Nama Lengkap') }}</label>
+                                <div class="@error('fullname')border border-danger rounded-3 @enderror">
+                                    <input class="form-control" type="text" placeholder="Nama Lengkap" id="fullname" value="{{ old('fullname') }}">
+                                        @error('fullname')
+                                            <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                        @enderror
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="username" class="form-control-label">{{ __('User Name') }}</label>
+                                <div class="@error('username')border border-danger rounded-3 @enderror">
+                                    <input class="form-control" type="text" placeholder="User Name" id="username" value="{{ old('username') }}">
+                                        @error('username')
+                                            <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                        @enderror
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="email" class="form-control-label">{{ __('Email') }}</label>
+                                <div class="@error('email')border border-danger rounded-3 @enderror">
+                                    <input class="form-control" type="email" placeholder="user@example.com" id="email" value="{{ old('email') }}">
+                                        @error('email')
+                                            <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                        @enderror
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="phone" class="form-control-label">{{ __('Phone') }}</label>
+                                <div class="@error('phone')border border-danger rounded-3 @enderror">
+                                    <input class="form-control" type="text" placeholder="40770888444" id="phone" name="phone" value="{{ old('phone') }}">
+                                        @error('phone')
+                                            <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                        @enderror
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="password" class="form-control-label">{{ __('Password') }}</label>
+                                <div class="@error('password') border border-danger rounded-3 @enderror">
+                                    <input class="form-control" type="password" placeholder="Masukan Password" id="password" >
+                                    @error('password')
+                                        <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="password_confirmation" class="form-control-label">{{ __('Konfirmasi Password') }}</label>
+                                <div class="@error('password_confirmation') border border-danger rounded-3 @enderror">
+                                    <input class="form-control" type="password" placeholder="Masukan Ulang Password" id="password_confirmation">
+                                    @error('password_confirmation')
+                                        <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="user_type">Role</label>
+                                <select class="form-control" id="user_type" >
+                                    <option value="ketua" {{ old('user_type')=='ketua' ? 'selected' : '' }}>Ketua</option>
+                                    <option value="bendahara" {{ old('user_type')=='bedahara' ? 'selected' : ''  }}>Bendahara</option>
+                                    <option value="sekretaris" {{ old('user_type')=='sekretaris' ? 'selected' : ''  }}>Sekretaris</option>
+                                </select>
+                                @error('user_type')
+                                    <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    {{-- <div class="form-group">
+                        <label for="about">{{ 'About Me' }}</label>
+                        <div class="@error('user.about')border border-danger rounded-3 @enderror">
+                            <textarea class="form-control" id="about" rows="3" placeholder="Say something about yourself" name="about_me">{{ auth()->user()->about_me }}</textarea>
+                        </div>
+                    </div> --}}
+            </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn bg-gradient-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn bg-gradient-dark">Save changes</button>
+                </div>
+            </form>
+            </div>
+        </div>
+
     </div>
 
     <div class="row">
